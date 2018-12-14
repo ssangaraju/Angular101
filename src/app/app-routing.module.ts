3375ce55e23641111b378190e4d7bb6bb6321067
Sasidhar Sangaraju <sasidhar99@hotmail.com>
2018-12-14 08:21:32 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'home', component: HomeComponent},
+{path: 'welcome', component: WelcomeComponent},
+{path: 'about', component: AboutComponent}
+
+
+];
 
 @NgModule({
+  declarations:[HomeComponent, WelcomeComponent, AboutComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Sasidhar Angular Playground!';
+  title = 'Sasidhar Angular Patio de Recreo!!!';
 }
